00a4939e5289364979422dd897d3d00e2b492e71
ohiofi <20hz20khz@gmail.com>
2026-05-19 10:50:31 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,7 +1,17 @@
 public class Map {
     private Room[][] gameMap;
 
+    // 5 x 5 grid, 25 rooms
+    // 1 spawn point at middle of map [row 2, col 2]
+    // 4 temples
+    // 4 keys
+    // 5 rivers
+    // 2 healing rooms
+    // 6 ambush rooms
+    // 3 boring, nothing rooms
+
     public Map(){
+        gameMap = new Room[5][5];
         
     }
 }
